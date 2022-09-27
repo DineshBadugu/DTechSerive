952677290c966d1483f3fa90999c9953d018c40f
DineshBadugu <badugudinesh94@gmail.com>
2022-09-27 19:58:28 +0530

    fixed isuess on deployed heroku

--- DTechService/DTechService/urls.py
+++ DTechService/DTechService/urls.py
@@ -17,16 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',include('TechApp.urls')),
-
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
-
+#  from django.conf import settings
+# from django.conf.urls.static import static
+# from django.views.static import serve
+#  path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+#  path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
